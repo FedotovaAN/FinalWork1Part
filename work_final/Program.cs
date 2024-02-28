045d701753d0f04cdb81ffd33c574e5493723e92
Alla Fedotova <fedotovaan@bk.ru>
2024-02-29 00:43:56 +0300

    changed file program.cs

--- work_final/Program.cs
+++ work_final/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿﻿﻿string[] SelectRows(string[] firstArray, int knowLength)
+{
+    int i = 0;
+    int k = 0;
+    while (i<firstArray.Length)
+    {
+        if (firstArray[i].Length<=knowLength)
+            k++;
+        i++;
+    }
+    string[] secondArray = new string[k];
+    i = 0;
+    int n = 0;
+    while (i<firstArray.Length)
+    {
+        if (firstArray[i].Length<=knowLength)
+        {
+            secondArray[n] = firstArray[i];
+            n++;
+        }
+        i++;
+    }
+    return secondArray;
+}
+void PrintStringArray(string[] array)
+{
+    int l = 0;
+    System.Console.Write("[");
+    for (l=0; l<array.Length; l++)
+    {
+        System.Console.Write($"\"{array[l]}\"");
+        if (l!=array.Length-1)
+            System.Console.Write(", ");
+    }
+    System.Console.WriteLine("]");
+}
+
+// Для тестирования программы дан массив. 
+
+
+string[] stringArray = new string[] { "1",":-)", "12345", "Кошка"};
+
+string[] newStringArray = SelectRows(stringArray, 3);
+System.Console.WriteLine("Массив строк на входе:");
+PrintStringArray(stringArray);
+System.Console.WriteLine("Массив строк на выходе:");
+PrintStringArray(newStringArray);
